bad74e4e3b93d9473e625abc2f973287be90b452
sadegh hosseini <thesadegh.h@gmail.com>
2021-11-19 21:33:59 +0330

    add functionalty for buttons in movie page

--- src/components/movie page poster/MoviePageInfo.jsx
+++ src/components/movie page poster/MoviePageInfo.jsx
@@ -5,10 +5,12 @@ import MovieImage from '../../lib/movie image/movieImage';
 import { truncate } from '../../lib/truncate/truncate';
 import styles from './MoviePageInfo.module.css';
 import logo from '../../n1.png';
+import { useHistory } from 'react-router';
 
 function MoviePageInfo(props) {
   const movie = props.movieInfo;
   const image_BaseUrl = "https://image.tmdb.org/t/p/original";
+  const history = useHistory();
 
   const [width, height] = useWindowSize();
 
@@ -23,8 +25,8 @@ function MoviePageInfo(props) {
             <img className={styles.navBarLogo} src={logo} alt="NetflixLogo" />
         }
         <div>
-          <Button>JOIN NOW</Button>
-          <Button>SIGN IN</Button>
+          <Button onClick={() => history.push('/')}>JOIN NOW</Button>
+          <Button onClick={() => history.push('/logIn')} >SIGN IN</Button>
         </div>
       </div>
       <div className={styles.movieContainer}>
@@ -64,7 +66,7 @@ function MoviePageInfo(props) {
                   <img className={styles.footerLogo} src={logo} alt="logo" />
                   <p>Watch all you want.</p>
                 </div>
-                <Button className={styles.joinBtn}>JOIN NOW</Button>
+                <Button onClick={() => history.push('/')} className={styles.joinBtn}>JOIN NOW</Button>
               </div>
             </div> :
             <div className={styles.footer}>
@@ -72,7 +74,7 @@ function MoviePageInfo(props) {
                 <img className={styles.footerLogo} src={logo} alt="logo" />
                 <div className={styles.mobileContent}>
                   <p>Watch all you want.</p>
-                  <Button className={styles.joinBtn}>JOIN NOW</Button>
+                  <Button onClick={() => history.push('/')} className={styles.joinBtn}>JOIN NOW</Button>
                 </div>
               </div>
             </div>
